f77145d4953fc136bcf3bcca1d52f4ea206395e5
Lek Tin <lek_tin@yahoo.com>
2016-05-13 08:31:04 +0800

    added checking.js

---
+++ checking.js
@@ -0,0 +1,14 @@
+var array = {
+	'item_1': 'item_1 value',
+	'item_2': 'item_2 value',
+	'item_3': 'item_3 value',
+	'item_4': 'item_4 value',
+	'item_5': 'item_5 value'
+};
+
+for (key in array) {
+	// Pass only if 'key' exists in 'array'
+	if (array.hasOwnProperty(key) {
+		// Do stuff
+	})
+}
