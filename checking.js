0d3b2b5d8a927888dde5a9db7c682432100938b0
lek_tin@yahoo.com <lek_tin@yahoo.com>
2016-08-05 18:49:36 +0800

    added maxNumberOfDigits and isAlphaNumeric functions

--- checking.js
+++ checking.js
@@ -1,4 +1,4 @@
-var array = {
+var sample_array = {
 	'item_1': 'item_1 value',
 	'item_2': 'item_2 value',
 	'item_3': 'item_3 value',
@@ -6,15 +6,31 @@ var array = {
 	'item_5': 'item_5 value'
 };
 
-for (key in array) {
-	// Pass only if 'key' exists in 'array'
-	if (array.hasOwnProperty(key) {
-		// Do stuff
-	})
+var keyExistInArray = function(array) {
+	for (key in array) {
+		// Pass only if 'key' exists in 'array'
+		if (array.hasOwnProperty(key) {
+			// Do stuff
+			console.log('Key indeed exists.');
+		})
+	}
 }
+
+keyExistInArray(sample_array);
 
 var if_console = function() {
 	if ( console ) {
 		return console;
 	}
+}
+
+var maxNumberOfDigits = function(target, num) {
+	if (target.value.length > num) {
+		target.value = target.value.slice(0, num);
+	}
+}
+
+var isAlphaNumeric = function(string) {
+	var myRegEx  = /[^a-z\d]/i;
+	return !(myRegEx.test(string));
 }
--- url.js
+++ url.js
@@ -6,12 +6,13 @@
 var getAbsoluteUrl = (function() {
 	var a;
 	return function(url) {
-		if(!a) a = document.createElement('a');
+		if(!a) {
+			a = document.createElement('a');	
+		}
 		a.href = url;
-
 		return a.href;
 	};
 })();
 
 // Usage
-getAbsoluteUrl('/something'); // https://davidwalsh.name/something
+getAbsoluteUrl('/something'); // http://domain.name/something
